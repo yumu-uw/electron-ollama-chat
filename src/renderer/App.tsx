29fc97132627f1e7af3329419341b64d0e0c3669
yumuuw <yumuuw@gmail.com>
2025-03-10 20:57:27 +0900

    muiをインストール

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,3 +1,4 @@
+import { Typography } from "@mui/material";
 import { useEffect } from "react";
 import { Link } from "react-router";
 
@@ -7,7 +8,9 @@ export const App = () => {
   });
   return (
     <div>
-      <h1>Hello, Electron!</h1>
+      <Typography variant="h1" gutterBottom>
+        h1. Heading
+      </Typography>
       <p>hoge</p>
       <Link to="/app2">App2 Page</Link>
     </div>
--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -4,6 +4,11 @@ import { App } from "./App"
 import { HashRouter, Route, Routes } from "react-router"
 import { App2 } from "./App2";
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <HashRouter>
