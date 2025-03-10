c80ffda6f8ca9c68f6d6e9848a1caa2837f3b546
yumuuw <yumuuw@gmail.com>
2025-03-10 20:46:57 +0900

    react-routerを追加

--- src/renderer/App2.tsx
+++ src/renderer/App2.tsx
@@ -1,15 +1,15 @@
 import { useEffect } from "react";
 import { Link } from "react-router";
 
-export const App = () => {
+export const App2 = () => {
   useEffect(() => {
     console.log(window.versions.chrome());
   });
   return (
     <div>
-      <h1>Hello, Electron!</h1>
-      <p>hoge</p>
-      <Link to="/app2">App2 Page</Link>
+      <h1>App2</h1>
+      <p>huga</p>
+      <Link to="/">App Page</Link>
     </div>
   );
 }
--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,9 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from 'react-dom/client'
 import { App } from "./App"
+import { HashRouter, Route, Routes } from "react-router"
+import { App2 } from "./App2";
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="app2" element={<App2 />} />
+      </Routes>
+    </HashRouter>
   </StrictMode>,
 );
